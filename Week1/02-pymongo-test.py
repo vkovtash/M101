ee2fcc8554a402fd44bfe1dc14fceca7380afb87
vkovtash <v.kovtash@gmail.com>
2012-10-24 19:01:07 +0400

    Initial Commit

---
+++ Week1/02-pymongo-test.py
@@ -0,0 +1,11 @@
+import pymongo
+
+from pymongo import Connection
+
+connection = Connection('localhost',27017)
+
+db = connection.test
+names = db.names
+item = names.find_one()
+
+print item['name']
---
+++ Week1/03-hello-world-mongo.py
@@ -0,0 +1,19 @@
+__author__ = 'kovtash'
+
+import bottle
+import pymongo
+
+@bottle.route('/')
+def index ():
+    from pymongo import Connection
+    connection = Connection('localhost',27017)
+
+    db = connection.test
+    names = db.names
+
+    item = names.find_one()
+
+    return '<b>Hello %s!</b>' % item['name']
+
+
+bottle.run(host='localhost',port=8080)
